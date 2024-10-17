23d17fcb16dd281f515dba8dbc941a7b191b5588
aloukikattrey <75660465+aloukikattrey@users.noreply.github.com>
2024-10-17 08:58:38 +0000

    changed index

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.send('Hello aloukik!');
 });
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
