4b97b3d94df8c2e06558945a19f843096bab0d25
aloukikattrey <75660465+aloukikattrey@users.noreply.github.com>
2024-10-18 06:35:12 +0000

    modified index

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import express from 'express';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello aloukik!');
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
